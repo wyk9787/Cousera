1374ffeeb1b44ddc0cd9dad7e88536347a988337
wyk9787 <wangyika@grinnell.edu>
2016-12-10 16:31:49 -0600

    twitter app done

--- Networking and Security in iOS Applications/W1/TwitterShareProduct/TwitterShareProduct/ViewController.m
+++ Networking and Security in iOS Applications/W1/TwitterShareProduct/TwitterShareProduct/ViewController.m
@@ -7,11 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "Social/Social.h"
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextView *tweetTextView;
 
--(void)configureTweetTextView;
+- (void) configureTweetTextView;
+- (void) showAlertMessages:(NSString *) myMess;
 
 @end
 
@@ -29,10 +31,44 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void) showAlertMessages:(NSString *) myMessage{
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"TweetShare" message:myMessage preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Okay" style:UIAlertActionStyleDefault handler:nil]];
+    [self presentViewController:alertController animated:YES completion:nil];
+}
 - (IBAction)showShareAction:(id)sender {
+    if ([self.tweetTextView isFirstResponder])
+        [self.tweetTextView resignFirstResponder];
+    
+    UIAlertController *actionController = [UIAlertController alertControllerWithTitle:@"" message:@ "Tweet your note" preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleDefault handler:nil];
+    
+    UIAlertAction *tweetAction = [UIAlertAction actionWithTitle:@"Tweet" style:UIAlertActionStyleDefault handler:
+                                  ^(UIAlertAction *action){
+                                      if([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter]){
+                                          SLComposeViewController *twitterVC  = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+                                          if([self.tweetTextView.text length] < 140){
+                                              [twitterVC setInitialText:self.tweetTextView.text];
+                                          }
+                                          else{
+                                              NSString *shortText = [self.tweetTextView.text substringToIndex:140];
+                                              [twitterVC setInitialText:shortText];
+                                          }
+                                          [self presentViewController:twitterVC animated:YES completion:nil];
+                                      }
+                                      else{
+                                          [self showAlertMessages: @"Please sign in tweeter before you tweet"];
+                                      }
+                                  }]; 
+    
+    [actionController addAction:tweetAction];
+    [actionController addAction:cancelAction];
+    
+    [self presentViewController:actionController animated:YES completion:Nil];
 }
 
--(void)configureTweetTextView{
+- (void) configureTweetTextView{
     self.tweetTextView.layer.backgroundColor = [UIColor colorWithRed:1.0 green:1.0 blue:0.9 alpha:1.0].CGColor;
     self.tweetTextView.layer.cornerRadius = 10.0;
     self.tweetTextView.layer.borderColor = [UIColor colorWithWhite:0 alpha:0.5].CGColor;
