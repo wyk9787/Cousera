2a618d4c1f39d2a2a4fc5f51158b063132ef95aa
wyk9787 <wangyika@grinnell.edu>
2016-12-13 22:32:39 -0600

    not get callback from the week2 instagram function

--- iOSDevelopmentForCreativeEntrepreneurs/Networking and Security in iOS Applications/W2/InstagramPlus/InstagramPlus/AppDelegate.m
+++ iOSDevelopmentForCreativeEntrepreneurs/Networking and Security in iOS Applications/W2/InstagramPlus/InstagramPlus/AppDelegate.m
@@ -11,31 +11,44 @@
 
 @interface AppDelegate ()
 
+@property (atomic) NSString *outgoingRedirect;
+@property (atomic) NSString *incomingRedirect;
+
 @end
 
 @implementation AppDelegate
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.outgoingRedirect = @"http://djp3.westmont.edu/classes/2015-coursera-live/redirect.php/myscheme/thing.com";
+    self.incomingRedirect = @"myscheme://thing.com";
     [[NXOAuth2AccountStore sharedStore] setClientID:@"3381b75adea44250aa29dacc59746448"
                                              secret:@"59d9d93028dc44d7a1304ca30002ec40"
                                    authorizationURL:[NSURL URLWithString:@"https://api.instagram.com/oauth/authorize/"]
                                            tokenURL:[NSURL URLWithString:@"https://api.instagram.com/oauth/access_token/"]
-                                        redirectURL:[NSURL URLWithString:@"https://is.gd/92D3PD"]
+                                        redirectURL:[NSURL URLWithString:self.outgoingRedirect]
                                      forAccountType:@"Instagram"];
     return YES;
 }
 
+- (BOOL) application:(UIApplication *)app oepnURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options{
+    NSLog(@"Succeed!");
+    if([self.incomingRedirect containsString:[url scheme]] && [self.incomingRedirect containsString:[url host]]){
+        NSURL *constructed = [NSURL URLWithString:[NSString stringWithFormat:@"%@?%@", self.outgoingRedirect, [url query]]];
+        return [[NXOAuth2AccountStore sharedStore] handleRedirectURL:constructed];
+    }
+    else{
+        return [[NXOAuth2AccountStore sharedStore] handleRedirectURL:url];
+    }
+    
+}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
 }
 
-- (BOOL) application:(UIApplication *)app oepnURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options{
-    NSLog(@"We reveived a callback");
-    return [[NXOAuth2AccountStore sharedStore] handleRedirectURL:url];
-}
+
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
--- iOSDevelopmentForCreativeEntrepreneurs/Networking and Security in iOS Applications/W2/InstagramPlus/InstagramPlus/ViewController.m
+++ iOSDevelopmentForCreativeEntrepreneurs/Networking and Security in iOS Applications/W2/InstagramPlus/InstagramPlus/ViewController.m
@@ -20,15 +20,86 @@
 @implementation ViewController
 - (IBAction)logininButtonPressed:(id)sender {
     [[NXOAuth2AccountStore sharedStore] requestAccessToAccountWithType:@"Instagram"];
+    self.logoutButton.enabled = true;
+    self.refreshButton.enabled = true;
+    self.loginButton.enabled = false;
 }
 - (IBAction)logoutButtonPressed:(id)sender {
+    NXOAuth2AccountStore *store = [NXOAuth2AccountStore sharedStore];
+    NSArray *instagramAccounts = [store accountsWithAccountType:@"Instagram"];
+    for(id acct in instagramAccounts){
+        [store removeAccount:acct];
+    }
+    self.logoutButton.enabled = false;
+    self.refreshButton.enabled = false;
+    self.loginButton.enabled = true;
 }
 - (IBAction)refreshButtonPressed:(id)sender {
+    NSArray *instagramAccounts = [[NXOAuth2AccountStore sharedStore] accountsWithAccountType:@"Instagram"];
+    if([instagramAccounts count] == 0){
+        NSLog(@"Warning: %ld Instagram accounts logged in", (long)[instagramAccounts count]);
+        return;
+    }
+    
+    NXOAuth2Account *acct = instagramAccounts[0];
+    NSString *token = acct.accessToken.accessToken;
+    NSString *urlStr = [@"https://api.instagram.com/v1/users/self/media/recent/?access_token=" stringByAppendingString:token];
+    NSURL *url = [NSURL URLWithString:urlStr];
+    
+    NSURLSession *session = [NSURLSession sharedSession];
+    [[session dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error){
+        
+        //check for network error
+        if(error){
+            NSLog(@"Couldn't finish the request: %@", error);
+            return;
+        }
+        
+        //check for http error
+        NSHTTPURLResponse *httpResp = (NSHTTPURLResponse *)response;
+        if (httpResp.statusCode < 200 || httpResp.statusCode >= 300){
+            NSLog(@"Error: Got status code %ld", (long)httpResp.statusCode);
+            return;
+        }
+        
+        //check for parsing error
+        NSError *parseErr;
+        id pkg = [NSJSONSerialization JSONObjectWithData:data options:0 error:&parseErr];
+        if(!pkg){
+            NSLog(@"Error: Couldn't parse response: %@", parseErr);
+        }
+        
+        NSString *imageURLstr = pkg[@"data"][0][@"images"][@"standard_resolution"][@"url"];
+        NSURL *imageURL = [NSURL URLWithString:imageURLstr];
+        [[session dataTaskWithURL:imageURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error){
+            
+            //check for network error
+            if(error){
+                NSLog(@"Couldn't finish the request: %@", error);
+                return;
+            }
+            
+            //check for http error
+            NSHTTPURLResponse *httpResp = (NSHTTPURLResponse *)response;
+            if (httpResp.statusCode < 200 || httpResp.statusCode >= 300){
+                NSLog(@"Error: Got status code %ld", (long)httpResp.statusCode);
+                return;
+            }
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.imageView.image = [UIImage imageWithData:data];
+            });
+            
+        }]resume];
+        
+    }] resume];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.logoutButton.enabled = false;
+    self.refreshButton.enabled = false;
 }
 
 
