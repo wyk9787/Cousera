56348837d5ad5d8658422185b08322ee3456b0f0
wyk9787 <wangyika@grinnell.edu>
2016-12-26 17:57:09 -0600

    week2 half done

--- DataStructuresAndAlgorithms/DataStructures/W2/Programming-Assignment-2/hw/job_queue.cpp
+++ DataStructuresAndAlgorithms/DataStructures/W2/Programming-Assignment-2/hw/job_queue.cpp
@@ -16,6 +16,13 @@ class JobQueue {
   vector<int> assigned_workers_;
   vector<long long> start_times_;
 
+  struct i_d{
+    long long id;
+    long long free;
+  };
+
+  typedef struct i_d unit;
+
   void WriteResponse() const {
     for (int i = 0; i < jobs_.size(); ++i) {
       cout << assigned_workers_[i] << " " << start_times_[i] << "\n";
@@ -30,21 +37,31 @@ class JobQueue {
       cin >> jobs_[i];
   }
 
+  unit my_greater(const unit a, const unit b){
+   return a.free > b.free ? a : b;
+  }
+
   void AssignJobs() {
     // TODO: replace this code with a faster algorithm.
-    assigned_workers_.resize(jobs_.size());
-    start_times_.resize(jobs_.size());
-    vector<long long> next_free_time(num_workers_, 0);
-    for (int i = 0; i < jobs_.size(); ++i) {
-      int duration = jobs_[i];
-      int next_worker = 0;
-      for (int j = 0; j < num_workers_; ++j) {
-        if (next_free_time[j] < next_free_time[next_worker])
-          next_worker = j;
+    priority_queue<unit, std::vector<unit>, std::greater<unit> > q();
+    for(int i = 0; i < m; i++){
+      unit u;
+      u.id = i;
+      u.free = 0;
+      q.push_back(u);
+    }
+    int j = 0;
+    for(int i = 0; i < m; i++){
+      while(q.top().id <= i){
+        assigned_workers_.push_back(q.top().id);
+        start_tiems_.push_back(i);
+        unit temp;
+        temp.id = q.top().id;
+        temp.free = q.top().free + jobs_[j];
+        q.pop();
+        q.push(temp);
+        j++;
       }
-      assigned_workers_[i] = next_worker;
-      start_times_[i] = next_free_time[next_worker];
-      next_free_time[next_worker] += duration;
     }
   }
 
