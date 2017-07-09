50f3f1502cbb561c95580a6ea939df83806c4ff5
wyk9787 <wangyika@grinnell.edu>
2017-07-09 02:42:41 -0500

    pass week2

--- MachineLearning/Week2/ex1/computeCost.m
+++ MachineLearning/Week2/ex1/computeCost.m
@@ -7,13 +7,14 @@ function J = computeCost(X, y, theta)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
-
+h = X * theta;
+error = h - y;
+error_sqr = error.^2;
+error_sum = sum(error_sqr);
+J = 1 / (2 * m) * error_sum;
 
 
 
--- MachineLearning/Week2/ex1/gradientDescent.m
+++ MachineLearning/Week2/ex1/gradientDescent.m
@@ -16,16 +16,11 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
-
+    theta = theta - (alpha / m) * X' * (X * theta - y);
     % ============================================================
 
     % Save the cost J in every iteration    
+    % fprintf("No. %d iteration: theta = %f\n", iter, theta);
     J_history(iter) = computeCost(X, y, theta);
 
 end
