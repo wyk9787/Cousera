46a5a13af05fade776d3fa85c2520e50df274899
wyk9787 <wangyika@grinnell.edu>
2017-07-17 15:46:54 -0500

    update 07/17

--- AppliedDataScienceWithPython/IntroductionToDataScienceInPython/Week2/Assignment2.py
+++ AppliedDataScienceWithPython/IntroductionToDataScienceInPython/Week2/Assignment2.py
@@ -2,21 +2,25 @@
 # coding: utf-8
 
 # ---
-# 
+#
 # _You are currently looking at **version 1.2** of this notebook. To download notebooks and datafiles, as well as get help on Jupyter notebooks in the Coursera platform, visit the [Jupyter Notebook FAQ](https://www.coursera.org/learn/python-data-analysis/resources/0dhYG) course resource._
-# 
+#
 # ---
 
 # # Assignment 2 - Pandas Introduction
 # All questions are weighted the same in this assignment.
 # ## Part 1
-# The following code loads the olympics dataset (olympics.csv), which was derrived from the Wikipedia entry on [All Time Olympic Games Medals](https://en.wikipedia.org/wiki/All-time_Olympic_Games_medal_table), and does some basic data cleaning. 
-# 
+# The following code loads the olympics dataset (olympics.csv), which was derrived from the Wikipedia entry on [All Time Olympic Games Medals](https://en.wikipedia.org/wiki/All-time_Olympic_Games_medal_table), and does some basic data cleaning.
+#
 # The columns are organized as # of Summer games, Summer medals, # of Winter games, Winter medals, total # number of games, total # of medals. Use this dataset to answer the questions below.
 
 # In[4]:
-
 import pandas as pd
+
+def print_full(x):
+    pd.set_option('display.max_rows', len(x))
+    print(x)
+    pd.reset_option('display.max_rows')
 
 df = pd.read_csv('olympics.csv', index_col=0, skiprows=1)
 
@@ -32,17 +36,17 @@ for col in df.columns:
 
 names_ids = df.index.str.split('\s\(') # split the index by '('
 
-df.index = names_ids.str[0] # the [0] element is the country name (new index) 
+df.index = names_ids.str[0] # the [0] element is the country name (new index)
 df['ID'] = names_ids.str[1].str[:3] # the [1] element is the abbreviation or ID (take first 3 characters from that)
-
+print("Input successfully\n")
 df = df.drop('Totals')
-df.head()
+print df.head()
 
 
 # ### Question 0 (Example)
-# 
+#
 # What is the first country in df?
-# 
+#
 # *This function should return a Series.*
 
 # In[2]:
@@ -56,12 +60,12 @@ def answer_zero():
 
 # You can examine what your function returns by calling it in the cell. If you have questions
 # about the assignment formats, check out the discussion forums for any FAQs
-answer_zero() 
-
+answer_zero()
+print("Answer zero successfully\n")
 
 # ### Question 1
 # Which country has won the most gold medals in summer games?
-# 
+#
 # *This function should return a single string value.*
 
 # In[19]:
@@ -75,7 +79,7 @@ answer_one()
 
 # ### Question 2
 # Which country had the biggest difference between their summer and winter gold medal counts?
-# 
+#
 # *This function should return a single string value.*
 
 # In[45]:
@@ -89,18 +93,18 @@ answer_two()
 
 
 # ### Question 3
-# Which country has the biggest difference between their summer gold medal counts and winter gold medal counts relative to their total gold medal count? 
-# 
+# Which country has the biggest difference between their summer gold medal counts and winter gold medal counts relative to their total gold medal count?
+#
 # $$\frac{Summer~Gold - Winter~Gold}{Total~Gold}$$
-# 
+#
 # Only include countries that have won at least 1 gold in both summer and winter.
-# 
+#
 # *This function should return a single string value.*
 
 # In[65]:
 
 def answer_three():
-    df_gold = df[(df['Gold'] >= 1) & (df['Gold.1'] >=1)] 
+    df_gold = df[(df['Gold'] >= 1) & (df['Gold.1'] >=1)]
     max_val = max((df_gold['Gold']-df_gold['Gold.1'])/(df_gold['Gold']+df_gold['Gold.1']))
     max_country = df_gold[(df_gold['Gold']-df_gold['Gold.1'])/(df_gold['Gold']+df_gold['Gold.1']) == max_val].index[0]
     return max_country
@@ -110,7 +114,7 @@ answer_three()
 
 # ### Question 4
 # Write a function that creates a Series called "Points" which is a weighted value where each gold medal (`Gold.2`) counts for 3 points, silver medals (`Silver.2`) for 2 points, and bronze medals (`Bronze.2`) for 1 point. The function should return only the column (a Series object) which you created.
-# 
+#
 # *This function should return a Series named `Points` of length 146*
 
 # In[68]:
@@ -123,12 +127,12 @@ answer_four()
 
 # ## Part 2
 # For the next set of questions, we will be using census data from the [United States Census Bureau](http://www.census.gov/popest/data/counties/totals/2015/CO-EST2015-alldata.html). Counties are political and geographic subdivisions of states in the United States. This dataset contains population data for counties and states in the US from 2010 to 2015. [See this document](http://www.census.gov/popest/data/counties/totals/2015/files/CO-EST2015-alldata.pdf) for a description of the variable names.
-# 
+#
 # The census dataset (census.csv) should be loaded as census_df. Answer questions using this as appropriate.
-# 
+#
 # ### Question 5
 # Which state has the most counties in it? (hint: consider the sumlevel key carefully! You'll need this for future questions too...)
-# 
+#
 # *This function should return a single string value.*
 
 # In[5]:
@@ -154,7 +158,7 @@ answer_five()
 
 # ### Question 6
 # Only looking at the three most populous counties for each state, what are the three most populous states (in order of highest population to lowest population)? Use `CENSUS2010POP`.
-# 
+#
 # *This function should return a list of string values.*
 
 # In[22]:
@@ -174,9 +178,9 @@ answer_six()
 
 # ### Question 7
 # Which county has had the largest absolute change in population within the period 2010-2015? (Hint: population values are stored in columns POPESTIMATE2010 through POPESTIMATE2015, you need to consider all six columns.)
-# 
+#
 # e.g. If County Population in the 5 year period is 100, 120, 80, 105, 100, 130, then its largest change in the period would be |130-80| = 50.
-# 
+#
 # *This function should return a single string value.*
 
 # In[167]:
@@ -185,7 +189,7 @@ def answer_seven():
     df_filtered = census_df[census_df['SUMLEV'] == 50]
     df_filtered = df_filtered.loc[:,['CTYNAME', 'POPESTIMATE2010', 'POPESTIMATE2011', 'POPESTIMATE2012', 'POPESTIMATE2013', 'POPESTIMATE2014', 'POPESTIMATE2015']]
     df_filtered = df_filtered.set_index('CTYNAME').reset_index()
-    
+
     state_df = pd.DataFrame()
     state_df['State'] = df_filtered['CTYNAME']
     state_df['Count'] = 0
@@ -199,10 +203,10 @@ answer_seven()
 
 
 # ### Question 8
-# In this datafile, the United States is broken up into four regions using the "REGION" column. 
-# 
+# In this datafile, the United States is broken up into four regions using the "REGION" column.
+#
 # Create a query that finds the counties that belong to regions 1 or 2, whose name starts with 'Washington', and whose POPESTIMATE2015 was greater than their POPESTIMATE 2014.
-# 
+#
 # *This function should return a 5x2 DataFrame with the columns = ['STNAME', 'CTYNAME'] and the same index ID as the census_df (sorted ascending by index).*
 
 # In[76]:
@@ -210,7 +214,7 @@ answer_seven()
 def answer_eight():
     df_filtered = census_df[census_df['SUMLEV'] == 50]
     df_index = df_filtered.reset_index()
-    
+
     ans_df = pd.DataFrame()
     ans_df['STNAME'] = ''
     ans_df['CTYNAME'] = ''
@@ -226,8 +230,7 @@ def answer_eight():
             state_name = df_index[df_index['index'] == index]['STNAME'].iloc[0]
             data = pd.DataFrame({'STNAME' : [state_name], 'CTYNAME' : [county_name]}, index=[index])
             ans_df = ans_df.append(data)
-    ans_df = ans_df[['STNAME', 'CTYNAME']]        
+    ans_df = ans_df[['STNAME', 'CTYNAME']]
     return ans_df
 
 answer_eight()
-
